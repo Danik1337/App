1092412d63326ef0edf02027bd98e462027d182d
Danya <6danisimo6@gmail.com>
2024-02-04 17:36:59 +0300

    Init commit

---
+++ lib/classes/App.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+  State<App> createState() => _HomeState();
+}
+
+class _HomeState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text("QWERRTT")
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:figma/classes/App.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: App()));
+}
